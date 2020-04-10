391dea858cc4112a4a0d2df52367541f31ea620a
Michael Simini <mako1393@gmail.com>
2020-04-10 11:19:27 -0300

    conceitos-reactjs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(res => {
+      setRepositories(res.data)
+    })
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+    const res = await api.post('repositories', {
+      title: 'test',
+      url: 'https://github.com/test',
+      techs: ['Java', 'Angular']
+    })
+
+    setRepositories([...repositories, res.data])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+
+    setRepositories(repositories.filter(repo=> repo.id !== id))
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repo => ( 
+        <li key={repo.id}>
+          {repo.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
+          <button onClick={() => handleRemoveRepository(repo.id)}>
             Remover
           </button>
         </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
